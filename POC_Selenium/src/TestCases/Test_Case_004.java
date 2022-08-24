64c079410652cb1c24123fda4c53fd7224f55616
KarenLPaz <110488067+KarenLPaz@users.noreply.github>
2022-08-24 13:24:41 -0500

    Test case 003, 004 and 005(missing some steps)

--- POC_Selenium/src/TestCases/Test_Case_004.java
+++ POC_Selenium/src/TestCases/Test_Case_004.java
@@ -4,17 +4,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Test_Case_002 {
+public class Test_Case_004 {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\klopezpaz\\Documents\\selenium\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver ();
         driver.manage().window().maximize();
-		driver.get("https://cms.demo.katalon.com/my-account");
-		driver.findElement(By.id("username")).sendKeys("customer");
-		driver.findElement(By.id("password")).sendKeys("crz7mrb.KNG3yxv");
-		driver.findElement(By.className("woocommerce-button")).click();
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.name("user-name")).sendKeys("locked_out_user");
+		driver.findElement(By.name("password")).sendKeys("secret_sauce");
+		driver.findElement(By.name("login-button")).click();
+		
+		//driver.findElement(By.id("react-burger-menu-btn")).click();
+		//driver.findElement(By.id("logout_sidebar_link")).click();
 		//driver.close();
+		
 
 	}
 
