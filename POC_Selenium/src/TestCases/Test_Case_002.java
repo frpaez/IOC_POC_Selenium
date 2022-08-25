9e9d0f19ace0ec5dd5b4dd6cc0b69568660087e7
KarenLPaz <110488067+KarenLPaz@users.noreply.github>
2022-08-25 09:55:24 -0500

    Test suite addition

--- POC_Selenium/src/TestCases/Test_Case_002.java
+++ POC_Selenium/src/TestCases/Test_Case_002.java
@@ -1,12 +1,13 @@
 package TestCases;
 
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test_Case_002 {
-
-	public static void main(String[] args) {
+	@Test 
+	public  void main() {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\klopezpaz\\Documents\\selenium\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver ();
         driver.manage().window().maximize();
@@ -14,7 +15,7 @@ public class Test_Case_002 {
 		driver.findElement(By.id("username")).sendKeys("customer");
 		driver.findElement(By.id("password")).sendKeys("crz7mrb.KNG3yxv");
 		driver.findElement(By.className("woocommerce-button")).click();
-		//driver.close();
+		driver.close();
 
 	}
 
